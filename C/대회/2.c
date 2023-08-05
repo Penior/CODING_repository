b19d2555a490772a0e791a9dce3425213e46ce37
Penior~ <gyejong000001@gmail.com>
2023-08-05 21:04:16 +0900

    2023. 08. 05 (3)

--- C/대회/2.c
+++ C/대회/2.c
@@ -1,14 +1,69 @@
 #include <stdio.h>
 #include <math.h>
 
-int K;
-int X;
-int count, sum = 0;
-int divide;
+int K, X, divide, min;
+int count, sum, digit = 0;
 
-int main (){
+int main () {
     scanf("%d", &K);
-    scanf("%d", &X);
-
+    char X0[K];
+    scanf("%s", &X0);
     
+    /* 이진법에서 1 위치에 따라 점수 부여 */
+    for (int i = 0; i < K; i++) {
+        if(X0[K - i - 1] == '1') {
+            if((K - i - 1) > digit) {
+                digit = (K - i); 
+                break;
+            }
+        }
+    }
+
+    min = count + digit;
+    
+    while (X0 != '{1}') {
+        digit = 0;
+
+        X0[K-1] += 1;
+
+        /* 이진법에서 1 더하기 */
+        for (int i = 0; i < K; i++) {
+            if(X0[K - i - 1] == '2') {
+                X0[K - i - 1] = '0';
+                X0[K - i - 2] += 1;
+            if (X0[0] == '2') {
+                X0[K+1] = '0';
+                X0[0] = '1';
+                }
+            }
+            if (X0[K - i - 2] == '1') {
+                break;
+            }
+        }
+
+        /* 이진법에서 1 위치에 따라 점수 부여 */
+        for (int i = 0; i < K; i++) {
+            if(X0[K - i - 1] == '1') {
+                if((K - i - 1) > digit) {
+                    digit = (K - i); 
+                    break;
+                }
+            }
+        }
+
+        count += 1;
+
+        if(count > min) {
+            break;
+        }
+
+        sum += 1;
+
+        if(count + digit < min) {
+            min = count + digit;
+        }
+
+    }  
+    
+    printf("%d", min);
 }
--- C/대회/test.c
+++ C/대회/test.c
@@ -1,28 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    int n;
-    printf("입력할 숫자의 개수를 입력하세요: ");
-    scanf("%d", &n);
+int N, M, j;
 
-    int* list = (int*)malloc(n * sizeof(int)); // 사용자가 입력한 개수만큼 메모리 할당
+int main() {
+    scanf("%d %d", &N, &M);
+    char InputStory[N], OutputStory[N];
 
-    printf("정수들을 공백을 포함하여 입력하세요: ");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &list[i]); // 숫자를 배열에 저장
+    for (int i = 0; i < N; i++) {
+        scanf("%[\n]s", &InputStory[i]);
     }
 
-    // 결과 출력
-    printf("입력한 숫자들을 배열에 저장했습니다:\n");
-    for (int i = 0; i < n; i++) {
-        printf("list[%d] = %d\n", i, list[i]);
-    }
-
-    // 동적 메모리 해제
-    free(list);
-
-    return 0;
 }
