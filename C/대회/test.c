95d9fad4f4f327fee90adec76703eba2b701304f
Penior~ <gyejong000001@gmail.com>
2023-08-05 22:23:12 +0900

    2023. 08. 05 (4)

--- C/대회/test.c
+++ C/대회/test.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-
-int N, M, j;
 
 int main() {
-    scanf("%d %d", &N, &M);
-    char InputStory[N], OutputStory[N];
-
-    for (int i = 0; i < N; i++) {
-        scanf("%[\n]s", &InputStory[i]);
-    }
-
+    char str[100];
+    printf("문자열을 입력하세요: ");
+    scanf("%s", str);
+    getchar(); // 개행 문자 소비
+    printf("입력한 문자열: %s\n", str);
+    return 0;
 }
